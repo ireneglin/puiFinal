e4edae9b36a8d406086fcb5aa3216684d6d0bf3a
Irene Lin <irenelin@Pumpkin-Butt.wv.cc.cmu.edu>
2019-11-23 13:09:57 -0500

    pulling location from user input data, displaying correct weather icon

--- funct.js
+++ funct.js
@@ -14,46 +14,53 @@ JS functionalities to do
 //display parsed GET info
 
 var apiKey = "YP9GsGpyqk2uJxUJyHXFAyx7HBZqpA2H";
+var locationURL = "http://dataservice.accuweather.com/locations/v1/cities/us/search";
+var forecastURL = "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/";
 
-//URL helper function
-function urlMaker(url, q) {
-	var newURL = url.concat("?apikey=", apiKey, "&q=", q);
-	//return newURL;
-	console.log(newURL);
+
+
+//pull user input from location textbox
+function locationInputGetter() {
+	var city = document.getElementById("cityLocationInput").value;
+	var newLocationURL = locationURL.concat("?apikey=", apiKey, "&q=", city);
+	return newLocationURL;
 }
-
 
 //get location key
 //GET City Search (results narrowed by countryCode), USA code = US
 //http://dataservice.accuweather.com/locations/v1/cities/{countryCode}/search
 //location key = key
 function getLocationKey() {
-	fetch('http://dataservice.accuweather.com/locations/v1/cities/us/search?apikey=YP9GsGpyqk2uJxUJyHXFAyx7HBZqpA2H&q=Pittsburgh') 
+	var url = locationInputGetter();
+	fetch(url) 
 		.then(response => response.json())
 		.then(data => {
 			console.log(data);
-			console.log("type:"+typeof data[0].Key +" key: "+data[0].Key);
+			//console.log("type:"+typeof data[0].Key +" key: "+data[0].Key);
 			getHourWeather(data[0].Key);
 		});
 }
 
 function hourWeatherDisplayHelper(iconNum) {
-	//document.getElementById("weatherIcon").classList.add("wi");
+	//delete all class before adding new classes
+	document.getElementById("weatherIcon").className = '';
+
+	//add new classes
+	document.getElementById("weatherIcon").classList.add("wi");
 	if (iconNum.length == 2) {
 		document.getElementById("weatherIcon").classList.add("icon-accu"+iconNum);
 	} else {
 		document.getElementById("weatherIcon").classList.add("icon-accu0"+iconNum);
+		console.log("iconNum: "+iconNum);
 	}
 }
 
 //get weather for next hour, needs location key
 function getHourWeather(locationKey) {
 	//GET 1 Hour of Hourly Forecasts
-	var imgSrc = "https://developer.accuweather.com/sites/default/files/";
-	var url = "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/";
-	var nu = url.concat(locationKey,"?apikey=", apiKey);
-	console.log("url:"+nu);
-	fetch(nu)
+	var newForecastURL = forecastURL.concat(locationKey,"?apikey=", apiKey);
+	console.log("url:"+newForecastURL);
+	fetch(newForecastURL)
 		.then(response => response.json())
 		.then(data => {
 		console.log(data);
