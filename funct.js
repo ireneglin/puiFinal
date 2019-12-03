69c396ace912523f413a39d1fd3f4a56e9ca2661
Irene Lin <irenelin@Pumpkin-Butt.wv.cc.cmu.edu>
2019-12-03 13:23:44 -0500

    async await function test

--- funct.js
+++ funct.js
@@ -196,7 +196,7 @@ function buildTracksToPlaylistURL() {
 }
 
 //add tracks to playlist playlistURI = 62BgcjjA68WDuafi7AtQ9y
-function addTracksToPlaylist() {
+async function addTracksToPlaylist() {
 	var url = buildTracksToPlaylistURL();
 	var access = "Bearer " + accessToken;
 	fetch(url, {
